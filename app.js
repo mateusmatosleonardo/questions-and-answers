ea7408d23e8b6ccb42ad6686080f9f7f88abb1d7
mateusmatosleonardo <m4teusmatos@gmail.com>
2021-07-11 19:14:08 -0300

    adding logic

--- app.js
+++ app.js
@@ -5,10 +5,29 @@ for (var i = 0; i < elements.length; i++) {
         let pronounced = e.target.value;
         if (pronounced == "correct"){
             
-            let el = e.target.previousElementSibling.style.backgroundColor = "green";
+            let parentNode = e.target.parentNode;
+            parentNode.style.backgroundColor = "green";
+
+            let els = parentNode.parentNode.querySelectorAll('[type=radio]');
+
+            for(var n=0; n < els.length; n++){
+                els[n].disable = true;
+            }
 
         }else if(pronounced == "incorrect"){
-            let el = e.target.previousElementSibling.style.backgroundColor = "red";
+
+            let parentNode = e.target.parentNode;
+            parentNode.style.backgroundColor = "red";
+
+            let els = parentNode.parentNode.querySelectorAll('[type=radio]');
+
+            for(var n=0; n < els.length; n++){
+                els[n].disable = true;
+            }
+
+            let correct = parentNode.parentNode.querySelector('[valeu = correct]');
+            correct.parentNode.style.backgroundColor = 'green';
+
         }
     })
 }
