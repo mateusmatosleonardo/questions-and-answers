75c1c08e37ba41b8737ca72cfd9729324e33f77b
mateusmatosleonardo <m4teusmatos@gmail.com>
2021-07-11 15:34:02 -0300

    testing

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 var elements = document.querySelectorAll('[type=radio]');
 
 for (var i = 0; i < elements.length; i++) {
-    elements[i].addEventListener('change', () => {
-    
+    elements[i].addEventListener('change', (e) => {
+        let pronounced = e.target.value;
+        if (pronounced == "correct"){
+            alert("acertou a pergunta");
+        }else if(pronounced == "incorrect"){
+            alert("errou a pergunta");
+        }
     })
 }
